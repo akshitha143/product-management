f389d0c8f1119d52221038e31b2d746a5e799d30
akshitha143 <akshithaoleti@gmail.com>
2025-09-09 13:30:48 +0530

    change footer to footers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footers/Footer";
 import HomePage from "./pages/home/Homepage";
 import ProductsPage from "./pages/products/ProductsPage";
 import AddProductPage from "./pages/addproduct/AddProductPage";
