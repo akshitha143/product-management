424b53ad90a586b1093994e3b9ff96db0e823dcd
akshitha143 <akshithaoleti@gmail.com>
2025-09-09 20:19:41 +0530

    new change

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: "https://product-management-sigma-one.vercel.app/",
-}));
+app.use(cors());
 
 app.use("/api/products", productRoutes);
 
