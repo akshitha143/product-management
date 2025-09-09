4288dd44f97f9e46d1857b111e608549d10f2a08
akshitha143 <akshithaoleti@gmail.com>
2025-09-09 13:38:02 +0530

    file name chanded for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 // src/App.jsx
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header/Header";
-import Footer from "./components/footers/Footer";
-import HomePage from "./pages/homepage/Homepage";
-import ProductsPage from "./pages/products/ProductsPage";
-import AddProductPage from "./pages/addproduct/AddProductPage";
-import EditProductPage from "./pages/editproduct/EditProductPage";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footers/Footer.jsx";
+import HomePage from "./pages/landing/Homepage.jsx";
+import ProductsPage from "./pages/products/ProductsPage.jsx";
+import AddProductPage from "./pages/addproduct/AddProductPage.jsx";
+import EditProductPage from "./pages/editproduct/EditProductPage.jsx";
 
 
 export default function App() {
