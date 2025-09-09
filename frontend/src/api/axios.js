9d59b9f3fbed86f240ac5fbdc9289a6d4d2e5474
akshitha143 <akshithaoleti@gmail.com>
2025-09-09 20:17:12 +0530

    changed endpoin

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://product-management-xowi.onrender.com" ,
+  baseURL: "https://product-management-xowi.onrender.com/api" ,
   headers: { "Content-Type": "application/json" },
 });
 
