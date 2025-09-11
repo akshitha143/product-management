4a90c0ec7c43833a81ba4b4bf212ed501dd92ec6
akshitha143 <akshithaoleti@gmail.com>
2025-09-11 12:09:16 +0530

    axios adjust

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://product-management-xowi.onrender.com/api" ,
   headers: { "Content-Type": "application/json" },
+
+
 });
 
 export default api;
